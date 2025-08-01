6e8ff480f5b0b19e2ce16ef72a910cb7e8450eef
Andrew Gregory <andrew.gregory@oracle.com>
2025-08-01 12:16:58 -0400

    feat: New features and layout

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Features
+* Options such as fonts and tooltips are in a Menu dropdown
+* Starting work on audit history and cache comparison
 
 
 ## [1.3.0-rc.11](https://github.com/agregory999/oci-policy-analysis/compare/v1.3.0-rc.10...v1.3.0-rc.11) (2025-08-01)
--- docs/conf.py
+++ docs/conf.py
@@ -1,47 +1,7 @@
-"""Sphinx configuration for automatic documentation generation."""
-
-import os
-import sys
-
-# Add source to path
-sys.path.insert(0, os.path.abspath('../src'))
-
-# Project info
-project = 'OCI Policy Analysis'
-copyright = '2025, Andrew Gregory'
+project = 'oci-policy-dg-viewer'
 author = 'Andrew Gregory'
-
-# Get version dynamically
-try:
-    from importlib.metadata import version
-
-    release = version('oci-policy-dg-viewer')
-except ImportError:
-    release = 'unknown'
-
-# Extensions
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'autoapi.extension',
-    'sphinx.ext.viewcode',
-]
-
-# AutoAPI settings - creates everything automatically
-autoapi_type = 'python'
-autoapi_dirs = ['../src']
-autoapi_root = 'api'
-autoapi_keep_files = False
-autoapi_add_toctree_entry = False
-autoapi_generate_api_docs = True
-
-# HTML theme
+release = '0.1.0'  # Update manually or via setuptools_scm
+extensions = ['sphinx_rtd_theme', 'autoapi.extension']
 html_theme = 'sphinx_rtd_theme'
-
-# Napoleon settings
-napoleon_google_docstring = True
-napoleon_use_param = True
-napoleon_use_rtype = True
-
-# Set the master document (AutoAPI will create this)
-master_doc = 'api/index'
+autoapi_dirs = ['../src/oci_policy_dg_viewer']  # Adjust path to your source code
+master_doc = 'index'
