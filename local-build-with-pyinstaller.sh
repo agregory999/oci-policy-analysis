bb51cef247637b535aef78a5944991e0930f17ff
Andrew Gregory <andrew.gregory@oracle.com>
2026-01-27 11:44:01 -0500

    chore: build script update

--- local-build-with-pyinstaller.sh
+++ local-build-with-pyinstaller.sh
@@ -100,7 +100,7 @@ print(matches[0] if matches else '')")
       --collect-all=sys \
       --copy-metadata fastmcp \
       --add-data "src/oci_policy_analysis/version.txt:oci_policy_analysis" \
-      --add-data "src/reference_data:reference_data" \
+      --add-data "src/oci_policy_analysis/logic/permissions:oci_policy_analysis/logic/permissions" \
       --add-binary="$PY_SO:." \
       src/oci_policy_analysis/main.py
 
@@ -130,7 +130,7 @@ EOF
       --collect-all sys \
       --copy-metadata fastmcp \
       --add-data "src/oci_policy_analysis/version.txt;oci_policy_analysis" \
-      --add-data "src/reference_data;reference_data" \
+      --add-data "src/oci_policy_analysis/logic/permissions;oci_policy_analysis/logic/permissions" \
       --add-binary "$PY_DLL;." \
       src/oci_policy_analysis/main.py
 
